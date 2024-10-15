f6ee18039d7f2751a5df2a8763fbb5c6463c8d82
Luke-Wilson404 <Wilson-L69@ulster.ac.uk>
2024-10-15 11:09:49 +0100

    Create Calculator.cs

---
+++ MyTestLibrary/Calculator.cs
@@ -0,0 +1,31 @@
+namespace MyTestLibrary
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            double result = 0;
+            try
+            {
+                result = a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred. {ex.Message}");
+            }
+            return a / b;
+        }
+    }
+}
